db390520812de844c77bb1783969836e3aeb00fc
Riya Sun <riya.sunn@gmail.com>
2022-10-30 23:05:35 +1300

    Route not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./routes/landing-page/landing-page.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+// import Todo from './routes/todo/to-do.component';
+import AddTodo from "./routes/add-todo/add-todo.component";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />}>
+        <Route index element={<Home />} />
+        <Route path="addtodo" element={<AddTodo />} />
+        <Route path="navigation" element={<Navigation />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
